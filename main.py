83ea01550b39601b445a64cc21790e28c5b65b93
Leo Varghese <leovarghese@Leos-MacBook-Pro-2.local>
2024-03-17 14:36:06 +0530

    Add IMDB search functionality to main.py

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import argparse
+
+from imdb_scrapper.controller import IMDBController
+
+
 def main():
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument("query", help="String to be queried in IMDB", type=str)
+    args = parser.parse_args()
+
+    controller = IMDBController()
+    movie_ids = controller.get_movie_ids(args.query)
+    for movie_id in movie_ids:
+        movie = controller.get_movie_details(movie_id)
+        print(movie)
 
 
 if __name__ == "__main__":
